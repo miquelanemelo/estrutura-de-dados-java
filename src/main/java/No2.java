4d34ec05dac8f890ba068a5db4f27c52b6b9156c
Miquelane Melo <miquelanepmelo@gmail.com>
2021-07-26 19:14:45 -0300

    pilha em java

--- src/main/java/No2.java
+++ src/main/java/No2.java
@@ -1,2 +1,39 @@
-package PACKAGE_NAME;public class No2 {
+public class No2 {
+
+    private int dado;
+    private int refNo;
+
+    public No2() {
+    }
+
+    public No2(int dado) {
+        this.dado = dado;
+    }
+
+    public int getDado() {
+        return dado;
+    }
+
+    public void setDado(int dado) {
+        this.dado = dado;
+    }
+
+    public void setRefNo(No2 refNo) {
+        this.refNo = refNo;
+    }
+
+    public int getRefNo() {
+        return refNo;
+    }
+
+    public void setRefDado(int refNo) {
+        this.refNo = refNo;
+    }
+
+    @Override
+    public String toString() {
+        return "No2{" +
+                "dado=" + dado +
+                '}';
+    }
 }
--- src/main/java/Pilha.java
+++ src/main/java/Pilha.java
@@ -1,2 +1,28 @@
-package PACKAGE_NAME;public class Pilha {
-}
+public class Pilha {
+
+    private No2 refNoEntradaPilha;
+
+    public Pilha() {
+        this.refNoEntradaPilha = null;
+    }
+    public void push(No2 novoNo){
+        No2 refAuxiliar = refNoEntradaPilha;
+        refNoEntradaPilha = novoNo;
+        refNoEntradaPilha.setRefNo(refAuxiliar);
+    }
+    public No2 pop(){
+        if(this.isEmpty()) {
+            No2 noPoped = refNoEntradaPilha;
+            refNoEntradaPilha = refNoEntradaPilha.getRefNo();
+
+        }return null;
+    }
+
+    public No2 top(){
+        return refNoEntradaPilha;
+    }
+
+    public boolean isEmpty(){
+            return refNoEntradaPilha == null ? true : false;
+    }
+    }
